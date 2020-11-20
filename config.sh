b89ed51bcba6a75deaf0b24e12828ba01ec0d768
Saurav Das <dassaurav644@gmail.com>
2020-11-20 11:34:04 +0530

    Added nighttab backup

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ echo "Adding files to repo"
 shopt -s expand_aliases
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 cd
-config add Screenshots .config/vis .config/alacritty .config/betterlockscreenrc .config/colorls .config/dunst .config/gtk-2.0 .config/gtk-3.0 .config/i3 .config/Kvantum .config/nvim .config/picom .config/polybar .config/ranger .config/rofi .config/zathura .zshrc .p10k.zsh README.md config.sh
+config add nightTab-backup.json Screenshots .config/vis .config/alacritty .config/betterlockscreenrc .config/colorls .config/dunst .config/gtk-2.0 .config/gtk-3.0 .config/i3 .config/Kvantum .config/nvim .config/picom .config/polybar .config/ranger .config/rofi .config/zathura .zshrc .p10k.zsh README.md config.sh
 config status
 echo "You can review current status above."
 read -p "Enter the commit message : " msg
